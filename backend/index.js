09acd8816f93b143a95d6577b711fb07bef07228
shreevardhini2812 <shreevardhini28122000@gmail.com>
2025-09-18 23:25:44 +0530

    login auth changes

--- backend/index.js
+++ backend/index.js
@@ -163,4 +163,4 @@ app.post("/api/reset/:token", async (req, res) => {
 });
 
 
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
